739a0d6a25f681b507f508b06cba844d25948cd2
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-07-02 17:47:02 +0530

    Update SigninMain.jsx

--- src/components/signin/SigninMain.jsx
+++ src/components/signin/SigninMain.jsx
@@ -15,51 +15,56 @@ function SigninMain() {
   const login = () => {
     const { email, password } = showUser;
 
-    if (password !== "" && email !== "" && email.indexOf('@') > -1) {
-      authCtx.login("dede", 1000)
-      redirect('/')
-    } else {
-      setError("Error")
-      // console.log("error")
-    }
-
+    // if (password !== "" && email !== "" && email.indexOf('@') > -1) {
+    //   authCtx.login("dede", 1000)
+    //   redirect('/')
+    // } else {
+    //   setError("Error")
+    // console.log("error")
   }
 
-  const PostData = async (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
+}
 
-    setUser({ ...showUser, [name]: value });
+const PostData = async (e) => {
+  const name = e.target.name;
+  const value = e.target.value;
+
+  if (name === "password") {
+
   }
+  if (name === "email") { }
 
-  return (
-    <div className='bg-black  bg-cover flex justify-center items-center h-[90vh]' style={{ backgroundImage: ` url(${BG})` }}>
-      <div className='flex flex-col justify-center items-center border-2 border-yellow-500 p-5 sm:p-10 rounded-xl backdrop-blur-2xl'>
-        <img className="w-16 rounded-full" src={KRS} alt="krsLogo" /><br />
-        <h1 className='text-white text-3xl font-bold'>Welcome to KRS</h1><br /><br />
-        <input
-          className='w-[300px] sm:w-[400px] bg-zinc-800 text-gray-300 p-1.5 text-lg rounded'
-          type="email"
-          name="email"
-          id="email"
-          placeholder='Enter email address'
-          onChange={PostData}
-        />
-        <br />
-        <input
-          className='w-[300px] sm:w-[400px] bg-zinc-800 text-gray-300 p-1.5 text-lg rounded'
-          type="password"
-          name="password"
-          id="password"
-          placeholder='Password'
-          onChange={PostData}
-        />
-        <h2 className='text-white leading-10'><Link to='/signin' >forgot password?</Link><Link style={{ color: "blue" }} to='/signup' > SignUp</Link></h2><br />
-        <button className='w-[200px] bg-yellow-500 text-lg rounded p-1.5 font-bold' onClick={login}>Log in</button>
-      </div>
-      {showError === "" ? "" : <Error msg={showError} />}
+  setUser({ ...showUser, [name]: value });
+}
+
+return (
+  <div className='bg-black  bg-cover flex justify-center items-center h-[90vh]' style={{ backgroundImage: ` url(${BG})` }}>
+    <div className='flex flex-col justify-center items-center border-2 border-yellow-500 p-5 sm:p-10 rounded-xl backdrop-blur-2xl'>
+      <img className="w-16 rounded-full" src={KRS} alt="krsLogo" /><br />
+      <h1 className='text-white text-3xl font-bold'>Welcome to KRS</h1><br /><br />
+      <input
+        className='w-[300px] sm:w-[400px] bg-zinc-800 text-gray-300 p-1.5 text-lg rounded'
+        type="email"
+        name="email"
+        id="email"
+        placeholder='Enter email address'
+        onChange={PostData}
+      />
+      <br />
+      <input
+        className='w-[300px] sm:w-[400px] bg-zinc-800 text-gray-300 p-1.5 text-lg rounded'
+        type="password"
+        name="password"
+        id="password"
+        placeholder='Password'
+        onChange={PostData}
+      />
+      <h2 className='text-white leading-10'><Link to='/signin' >forgot password?</Link><Link style={{ color: "blue" }} to='/signup' > SignUp</Link></h2><br />
+      <button className='w-[200px] bg-yellow-500 text-lg rounded p-1.5 font-bold' onClick={login}>Log in</button>
     </div>
-  )
+    {showError === "" ? "" : <Error msg={showError} />}
+  </div>
+)
 }
 
 export default SigninMain
