65d2111365c12dcebe0af38c37ffca68c6432075
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-07-02 17:39:33 +0530

    Update Error.jsx

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./CSS/Error.css";
 
-export default function Error() {
+export default function Error(props) {
     return (
         <div className="ErrorMdiv">Error</div>
     )
