e422c1b7c3057925c9782523ab587b4566be3c0c
Aditya Ranjan <aditya16ranjan@gmail.com>
2022-07-02 16:53:56 +0530

    signup

--- src/components/signin/SigninUp.jsx
+++ src/components/signin/SigninUp.jsx
@@ -11,14 +11,15 @@ function SigninUp() {
   const authCtx = useContext(AuthContext);
 
   const redirect=useNavigate();
-const login=()=>{
-  authCtx.login("dede",1000)
-  redirect('/')
+const signup=()=>{
+
+authCtx.login("dede",1000)
+redirect('/')
 }
 
   return (
-    <div className='bg-black  bg-cover flex justify-center items-center h-[92vh]' style={{backgroundImage: ` url(${BG})`}}>
-      <div style={{marginBottom:"100px"}} className='flex flex-col justify-center items-center border-2 border-yellow-500 p-5 sm:p-10 rounded-xl  backdrop-blur-2xl'>
+    <div className='bg-black  bg-cover flex justify-center items-center h-[100%]' style={{backgroundImage: ` url(${BG})`}}>
+      <div  className='flex mt-10 mb-10 flex-col justify-center items-center border-2 border-yellow-500 p-5 sm:p-10 rounded-xl  backdrop-blur-2xl'>
         <img className="w-16 rounded-full" src={KRS} alt="krsLogo" /><br />
         <h1 className='text-white text-3xl font-bold'>Sign Up</h1><br /><br />
         <input className='w-[300px] sm:w-[400px] bg-zinc-800 text-gray-300 p-1.5 text-lg rounded' type="text" name="" id="email" placeholder='Enter Name' /><br />
@@ -42,13 +43,25 @@ const login=()=>{
         <option value={"CSE"} >Medical Sciences</option>
         <option value={"CSE"} >Dental Sciences</option>
         <option value={"CSE"} >Nursing  Sciences</option>
-          </select><br />
+          </select>
+          <select className='w-[300px] sm:w-[400px] bg-zinc-800 text-gray-300 mb-5 p-1.5 text-lg rounded' name="year" id="email" >
+        <option selected disabled hidden>Select Year</option>
+        <option value={"CSE"} >1st</option>
+        <option value={"CSE"} >2nd</option>
+        <option value={"CSE"} >3rd</option>
+        <option value={"CSE"} >4th</option>
+       
+          </select>
         <input className='w-[300px] sm:w-[400px] bg-zinc-800 text-gray-300 p-1.5 text-lg rounded' type="password" name="" id="password" placeholder='Password' />
-      
-        <button className='w-[200px] bg-yellow-500 text-lg rounded p-1.5 font-bold' onClick={login}>Sign Up</button>
+        <br/>
+        <input className='w-[300px] sm:w-[400px] bg-zinc-800 text-gray-300 p-1.5 text-lg rounded' type="password" name="" id="password" placeholder='Confirm Password' />
+         <br/>
+        <button className='w-[200px] bg-yellow-500 text-lg rounded p-1.5 font-bold' onClick={signup}>Sign Up</button>
       </div>
     </div>
   )
 }
+
+
 
 export default SigninUp
