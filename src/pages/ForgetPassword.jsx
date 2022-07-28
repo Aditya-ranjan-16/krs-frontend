4d230bbb70afe031481f8889118e88f17e8bfdc4
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-07-28 12:08:09 +0530

    Update ForgetPassword.jsx

--- src/pages/ForgetPassword.jsx
+++ src/pages/ForgetPassword.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Footer from "../components/footer/Footer";
 import Navbar from "../components/header/Navbar";
+import BG from "../../public/dronebackground.jpg";
 
 export default function ForgetPassword() {
   return (
