9009a8c214b01fe9a3f3e0737de40313dbde3da0
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-07-29 01:08:25 +0530

    Update ForgetPassword.jsx

--- src/components/signin/ForgetPassword.jsx
+++ src/components/signin/ForgetPassword.jsx
@@ -42,7 +42,6 @@ export default function ForgetPassword() {
 
       if (resp.status == 200) {
         localStorage.setItem("email", JSON.stringify(email));
-
         navigate("/EnterOTP");
       }
     }
