1b4227b50b0d9de39e471b6329bbe34a6f62c1f7
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-07-29 00:34:03 +0530

    Update EnterOTP.jsx

--- src/pages/EnterOTP.jsx
+++ src/pages/EnterOTP.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Footer from "../components/footer/Footer";
+import Navbar from "../components/header/Navbar";
 
 export default function EnterOTP() {
   return <div>EnterOTP</div>;
