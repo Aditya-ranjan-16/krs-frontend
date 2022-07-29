de52201606c7a057ce849be7d063344ad3ac7278
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-07-30 00:26:26 +0530

    Create ChangePass.jsx

---
+++ src/pages/ChangePass.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Footer from "../components/footer/Footer";
+import Navbar from "../components/header/Navbar";
+
+export default function ChangePass() {
+  return (
+    <div>
+      <Navbar />
+      ChangePass
+      <Footer />
+    </div>
+  );
+}
