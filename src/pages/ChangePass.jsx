0998ba84d2129a1714d91fd6e81a4efee74454bd
Rishav Raj Singh <rishavrajcrj@gmail.com>
2022-07-30 09:29:43 +0530

    Update ChangePass.jsx

--- src/pages/ChangePass.jsx
+++ src/pages/ChangePass.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Footer from "../components/footer/Footer";
 import Navbar from "../components/header/Navbar";
+import ChangePassC from "./../components/signin/ChangePass";
 
 export default function ChangePass() {
   return (
     <div>
       <Navbar />
-      ChangePass
+      <ChangePassC />
       <Footer />
     </div>
   );
